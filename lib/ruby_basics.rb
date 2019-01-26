6ed728d7142fe6bf69dbacb5cc3ac91fc35774b7
Zachary <zbberman@yahoo.com>
2019-01-26 22:54:50 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,16 +1,16 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  name = value
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
